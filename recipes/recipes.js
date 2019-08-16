5259bc170edb0aad23c2af9ac51a3274e14c315f
BradenEM <bradenemason@gmail.com>
2019-08-16 11:36:25 -0500

    more endpoints built and working

--- recipes/recipes.js
+++ recipes/recipes.js
@@ -12,4 +12,49 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+	const {id} = req.params
+	const recipe = await db('recipes').where({id})
+
+	try {
+		res.status(200).json(recipe);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+})
+
+router.post("/", async (req, res) => {
+	const body = req.body;
+	const recipe = await db("recipes").insert(body)
+
+	try {
+		res.status(201).json(recipe);
+	} catch (error) {
+		res.status(500).json(error)
+	}
+})
+
+router.put("/:id", async (req, res) => {
+	const {id} = req.params;
+	const body = req.body;
+	const recipe = await db("recipes").update(body).where({id})
+
+	try {
+		res.status(200).json(recipe);
+	} catch (error) {
+		res.status(500).json(error)
+	}
+})
+
+router.delete("/:id", async (req, res) => {
+	const {id} = req.params;
+	const recipe = await db('recipes').where({id}).del();
+
+	try {
+		res.status(200).json(recipe);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+})
+
 module.exports = router;
