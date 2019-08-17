60157c78835f7e6ffd28fa1bdabb8209d1198b7d
Braden Mason <bradenemason@gmail.com>
2019-08-17 10:43:52 -0500

    created db with migrations and fixed ingredients file

--- ingredients/ingredients.js
+++ ingredients/ingredients.js
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
-const express = require('express');
-const db = require('../data/db-config');
+const express = require("express");
+const db = require("../data/db-config");
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  const ingredients = await db('ingredients');
+router.get("/", async (req, res) => {
+  const ingredients = await db("ingredients");
 
   try {
     res.status(200).json(ingredients);
@@ -14,20 +13,3 @@ router.get('/', async (req, res) => {
 });
 
 module.exports = router;
-=======
-const express = require('express');
-const db = require('../data/db-config');
-const router = express.Router();
-
-router.get('/', async (req, res) => {
-  const ingredients = await db('ingredients');
-
-  try {
-    res.status(200).json(ingredients);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
-
-module.exports = router;
->>>>>>> 70452322ae024cfd85199cdb5338a5cad3441474
